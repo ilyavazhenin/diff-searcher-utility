afeda2c4cf181a2250746ccf5eba97698408c870
Ilya Vazhenin <heyheyftw@gmail.com>
2022-12-03 22:47:49 +0400

    feat: add simple output of two json files as they are

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import _ from 'lodash';
+import fs from 'fs';
+
 const program = new Command();
 
-const takeFilePathsFromCLI = (path1, path2) => {
-  console.log(path1, path2, 'path1 and path2 here');
+const showDiff = (path1, path2) => {
+  console.log('\npath1 and path2 here and their contents:\n');
+  const file1Obj = JSON.parse(fs.readFileSync(path1));
+  const file2Obj = JSON.parse(fs.readFileSync(path2));
+  console.log(path1, '\n', file1Obj, '\n\n', path2, '\n', file2Obj);
 };
+
 
 program
   .name('gendiff')
@@ -12,6 +19,6 @@ program
   .version('0.0.1')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format')
-  .action(takeFilePathsFromCLI);
+  .action(showDiff);
 
 program.parse();
