9646ab80e2b69d49be42ef6cfd1569c3d5edc402
Ilya Vazhenin <heyheyftw@gmail.com>
2022-12-04 20:40:08 +0400

    feat: add sorted output diff as a string

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,64 +9,58 @@ import fs from 'fs';
 const program = new Command();
 
 const showDiff = (path1, path2) => {
-  console.log('\npath1 and path2 here and their contents:\n');
   const fileData1 = JSON.parse(fs.readFileSync(path1));
   const fileData2 = JSON.parse(fs.readFileSync(path2));
-  console.log(path1, '\n', fileData1, '\n\n', path2, '\n', fileData2);
 
   const compare = (obj1, obj2) => {
-    const arrayKeys1 = Object.keys(obj1);
-    const arrayKeys2 = Object.keys(obj2);
+    const arrayKeys1 = Object.keys(fileData1);
+    const arrayKeys2 = Object.keys(fileData2);
     const uniqueKeysFrom1 = _.difference(arrayKeys1, arrayKeys2);
-    const result = {};
+    const result = [];
 
-    // todo: расписать все в нормальные условия по ифам/свитчам, и возможно в один цикл.
-    for (let key of uniqueKeysFrom1) {
-      const keyValue = obj1[key];
-      key = `  - ${key}`;
-      result[key] = keyValue;
+    for (const key of uniqueKeysFrom1) {
+      const sign = '  - ';
+      result.push([key, obj1[key], sign]);
     }
 
     for (const key of arrayKeys1) {
-      if (arrayKeys2.includes(key) && obj2[key] === obj1[key]) {
-        const formattedKey = `    ${key}`;
-        result[formattedKey] = obj1[key];
+      if (arrayKeys2.includes(key)) {
+        if (obj2[key] === obj1[key]) {
+          const sign = '    ';
+          result.push([key, obj1[key], sign]);
+        }
+
+        if (obj2[key] !== obj1[key]) {
+          let sign = '  - ';
+          result.push([key, obj1[key], sign]);
+          sign = '  + ';
+          result.push([key, obj2[key], sign]);
+        }
       }
     }
 
-    for (const key of arrayKeys1) {
-      if (arrayKeys2.includes(key) && obj2[key] !== obj1[key]) {
-        let formattedKey = `  - ${key}`;
-        result[formattedKey] = obj1[key];
-        formattedKey = `  + ${key}`;
-        result[formattedKey] = obj2[key];
-      }
-    }
-
-    //  уникальные из второго массива, которых нет в первом
     for (const key of arrayKeys2) {
       if (!arrayKeys1.includes(key)) {
-        const formattedKey = `  + ${key}`;
-        result[formattedKey] = obj2[key];
+        const sign = '  + ';
+        result.push([key, obj2[key], sign]);
       }
     }
 
-    console.log('result:\n', result);
-    console.log('============');
-    console.log('{');
-    for (const key in result) {
-      if (key) console.log(`${key}: ${result[key]}`);
-    }
-    console.log('}');
-    return result;
+    const sortedResult = _.sortBy(result, [0]);
+
+    const resultArray = sortedResult.map((elem) => {
+      const [key, value, sign] = elem;
+      return `${sign}${key}: ${value}`;
+    });
+    resultArray.unshift('{');
+    resultArray.push('}');
+    const outputString = resultArray.join('\n');
+    console.log(result);
+    console.log(outputString);
+    return outputString;
   };
 
   compare(fileData1, fileData2);
-  // todo: не забыть отсортировать в конце вывод через sortBy из лодаша
-  // todo: сделать вывод из цикла for in в консоль лог нормальный, а не через кучу мутаций.
-  // То етсь елси поппадает под условие (нет в файле, есть в файле, разные значения и т д.
-  // Возможно!! то сразу выводим в консоль лог
-  // а еще лучше собрать структуру данных где есть ключ: + - = и значение keysign: {key: value})
 };
 
 program
