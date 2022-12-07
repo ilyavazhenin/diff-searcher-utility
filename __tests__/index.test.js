5a452d1d3b35efee290d2aada8c5d9f147697e2f
Ilya Vazhenin <heyheyftw@gmail.com>
2022-12-07 21:13:43 +0400

    feat: add more test cases on json comparing

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,6 @@
 import showDiff from '../src/index.js';
 
-test('flat objects, json', () => {
+test('flat objects, standard case, json', () => {
   const filepath1 = '__tests__/__fixtures__/file1.json';
   const filepath2 = '__tests__/__fixtures__/file2.json';
   const output = `{
@@ -10,6 +10,48 @@ test('flat objects, json', () => {
   - timeout: 50
   + timeout: 20
   + verbose: true
+}`;
+
+  expect(showDiff(filepath1, filepath2)).toEqual(output);
+});
+
+test('file 1 empty', () => {
+  const filepath1 = '__tests__/__fixtures__/file1-empty.json';
+  const filepath2 = '__tests__/__fixtures__/file2.json';
+  const output = `{
+  + host: hexlet.io
+  + timeout: 20
+  + verbose: true
+}`;
+
+  expect(showDiff(filepath1, filepath2)).toEqual(output);
+});
+
+test('files are the same', () => {
+  const filepath1 = '__tests__/__fixtures__/file1.json';
+  const filepath2 = '__tests__/__fixtures__/file2-equal-to-1.json';
+  const output = `{
+    follow: false
+    host: hexlet.io
+    proxy: 123.234.53.22
+    timeout: 50
+}`;
+
+  expect(showDiff(filepath1, filepath2)).toEqual(output);
+});
+// to do: write diffs with +
+test('same files, diff values', () => {
+  const filepath1 = '__tests__/__fixtures__/file1.json';
+  const filepath2 = '__tests__/__fixtures__/file2-diff-values.json';
+  const output = `{
+  - follow: false
+  + follow: true
+  - host: hexlet.io
+  + host: google.com
+  - proxy: 123.234.53.22
+  + proxy: 8.8.8.8
+  - timeout: 50
+  + timeout: 150
 }`;
 
   expect(showDiff(filepath1, filepath2)).toEqual(output);
--- src/index.js
+++ src/index.js
@@ -18,28 +18,24 @@ const makeOutputString = (arrayOfChanges) => {
 
 const compare = (obj1, obj2) => {
   const arrayKeys1 = Object.keys(obj1);
+  console.log(arrayKeys1);
   const arrayKeys2 = Object.keys(obj2);
+  console.log(arrayKeys2);
   const tempArray = [];
 
   for (const key of arrayKeys1) {
     if (arrayKeys2.includes(key)) {
-      if (obj2[key] === obj1[key]) {
-        tempArray.push([key, obj1[key], '    ']);
-      }
+      if (obj2[key] === obj1[key]) tempArray.push([key, obj1[key], '    ']);
 
       if (obj2[key] !== obj1[key]) {
         tempArray.push([key, obj1[key], '  - ']);
         tempArray.push([key, obj2[key], '  + ']);
       }
-    } else {
-      tempArray.push([key, obj1[key], '  - ']);
-    }
+    } else tempArray.push([key, obj1[key], '  - ']);
   }
 
   for (const key of arrayKeys2) {
-    if (!arrayKeys1.includes(key)) {
-      tempArray.push([key, obj2[key], '  + ']);
-    }
+    if (!arrayKeys1.includes(key)) tempArray.push([key, obj2[key], '  + ']);
   }
 
   const sortedChanges = _.sortBy(tempArray, [0]);
